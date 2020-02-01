658870bb290ec6001d5195fd3a57dca06aeeafa4
Benjamin Karstad <benjaminkarstad@WINDOWS2.SCS.CARLETON.CA>
2020-02-01 13:31:35 -0500

    improved swipe left and right animations;

--- Repair/Scripts/Card.gd
+++ Repair/Scripts/Card.gd
@@ -1,14 +1,17 @@
 extends Node2D
 
-
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
 
+func init(content, left_choice, right_choice):
+	set_content(content)
+	set_left_choice(left_choice)
+	set_right_choice(right_choice)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	self.change_text("Hello Testcase")
+	pass
 
 
 
@@ -16,5 +19,11 @@ func _ready():
 #func _process(delta):
 #	pass
 
-func change_text(text):
-	$RichTextLabel.text = text
+func set_content(text):
+	$Card/Content.text = text
+
+func set_left_choice(text):
+	$Card/LeftChoice.text = text
+
+func set_right_choice(text):
+	$Card/RightChoice.text = text
--- Repair/Scripts/CardSwipe.gd
+++ Repair/Scripts/CardSwipe.gd
@@ -4,23 +4,29 @@ var card_scene = preload("res://Scenes/Card.tscn")
 
 var deck = []
 
+var focused_card
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	_add_card()
-#	_add_card()
-#	_add_card()
-#	_add_card()
-#	_add_card()
-#	_add_card()
+	_add_card("Card1")
+	_add_card("Card2")
+	_add_card("Card3")
+	_add_card("Card4")
+	_add_card("Card5")
+	_add_card("Card6")
 	
 func _input(event):
 	if event.is_action_pressed("ui_left"):
 		_swipe("left")
 	elif event.is_action_pressed("ui_right"):
 		_swipe("right")
-		
-func _add_card():
+	
+func _add_card(content="", left="", right=""):
 	var new_card = card_scene.instance()
+	new_card.init(content, left, right)
+	
+	new_card.set_position(Vector2(375, 125))
+	
 	deck.push_front(new_card)
 	
 	add_child(new_card)
@@ -33,7 +39,7 @@ func _swipe(direction):
 	var top_card = _pop_card()
 	# do the top card's action
 	
-	var player = top_card.get_node("SwipeAnimations")
+	var player = top_card.get_node("Card/SwipeAnimations")
 	
 	if direction == "left":
 		player.play("SwipeLeft")
