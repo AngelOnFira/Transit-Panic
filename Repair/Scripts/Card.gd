84b3ef637f6dded3cee2add1cb26c66db9bd46c8
Benjamin Karstad <benkarstad@gmail.com>
2020-02-01 10:14:04 -0500

    added text for card choices;

--- Repair/Scripts/Card.gd
+++ Repair/Scripts/Card.gd
@@ -8,9 +8,13 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	self.change_text("Hello Testcase")
+
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
+
+func change_text(text):
+	$RichTextLabel.text = text
