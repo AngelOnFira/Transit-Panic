bc2fb09b1dca93620bf3490954094ca4880ddcbc
EnderZone <enzo.arenas1997@gmail.com>
2020-02-01 14:34:59 -0500

    Integrated GUI with Cards (accidently removed animations)

--- Repair/Scripts/Card.gd
+++ Repair/Scripts/Card.gd
@@ -3,11 +3,15 @@ extends Node2D
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
+var left_conseq
+var right_conseq
 
-func init(content, left_choice, right_choice):
+func init(content, left_choice, right_choice, left_cons, right_cons):
 	set_content(content)
 	set_left_choice(left_choice)
 	set_right_choice(right_choice)
+	set_left_conseq(left_cons)
+	set_right_conseq(right_cons)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -27,3 +31,15 @@ func set_left_choice(text):
 
 func set_right_choice(text):
 	$Card/RightChoice.text = text
+
+func set_left_conseq(text):
+	left_conseq = text
+
+func set_right_conseq(text):
+	right_conseq = text
+
+func get_left_conseq():
+	return left_conseq
+
+func get_right_conseq():
+	return right_conseq
--- Repair/Scripts/CardSwipe.gd
+++ Repair/Scripts/CardSwipe.gd
@@ -1,29 +1,39 @@
 extends Node2D
 
 var card_scene = preload("res://Scenes/Card.tscn")
+var res_eng = preload("ResourceEngine/ResourceEngine.gd").new()
 
 var deck = []
 
 var focused_card
 
+
+enum SWIPE {
+	right,
+	left
+}
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	_add_card("Card1")
-	_add_card("Card2")
-	_add_card("Card3")
-	_add_card("Card4")
-	_add_card("Card5")
-	_add_card("Card6")
+	_add_card("Card1", "Left text", "Right text", "$,100", "$,-100")
+	_add_card("Card2", "Left text", "Right text", "G,100", "O,-100")
+	_add_card("Card3", "Left text", "Right text", "$,100", "O,-100")
+	_add_card("Card4", "Left text", "Right text", "O,100", "$,-300")
+	_add_card("Card5", "Left text", "Right text", "O,100", "G,10")
+	_add_card("Card6", "Left text", "Right text", "$,100", "$,200")
+
+func _process(delta):
+	updateGUI()
 	
 func _input(event):
 	if event.is_action_pressed("ui_left"):
-		_swipe("left")
+		_swipe(SWIPE.left)
 	elif event.is_action_pressed("ui_right"):
-		_swipe("right")
+		_swipe(SWIPE.right)
 	
-func _add_card(content="", left="", right=""):
+func _add_card(content="", left="", right="", left_c="", right_c=""):
 	var new_card = card_scene.instance()
-	new_card.init(content, left, right)
+	new_card.init(content, left, right, left_c, right_c)
 	
 	new_card.set_position(Vector2(375, 125))
 	
@@ -37,14 +47,34 @@ func _pop_card():
 	
 func _swipe(direction):
 	var top_card = _pop_card()
-	# do the top card's action
 	
-	var player = top_card.get_node("Card/SwipeAnimations")
+	_play_card(direction, top_card)
 	
-	if direction == "left":
-		player.play("SwipeLeft")
-	elif direction == "right":
-		player.play("SwipeRight")
-		
-	yield(player, "animation_finished")
 	top_card.queue_free()
+
+func _play_card(dir, card) :
+	var player = card.get_node("Card/SwipeAnimations")
+	match dir:
+		SWIPE.right:
+			print("RIGHT")
+			player.play("SwipeRight")
+			res_eng.processConsequent(card.get_right_conseq())
+		SWIPE.left:
+			player.play("SwipeLeft")
+			print("LEFT")
+			res_eng.processConsequent(card.get_left_conseq())
+		_:
+			pass
+	yield(player, "animation_finished")
+	
+func updateGUI() :
+	var gui_container : VBoxContainer = self.get_node("GUI").get_child(0)
+	
+	var gold_text : Label = gui_container.get_child(0)
+	gold_text.text = "Gold: " + str(res_eng.getResourceValue("$"))
+	
+	var opinion_text : Label = gui_container.get_child(1)
+	opinion_text.text = "Opinion: " + str(res_eng.getResourceValue("O"))
+
+	var government_text : Label = gui_container.get_child(2)
+	government_text.text = "Government: " + str(res_eng.getResourceValue("G"))
