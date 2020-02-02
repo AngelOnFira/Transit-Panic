d03150d6707eaf7937a71ff872cf6fcc753260c9
Connor Hillen <connor.hillen@gmail.com>
2020-02-02 12:11:58 -0500

    Adding global variables for the end scene

---
+++ Repair/Scripts/PlayerVariables.gd
@@ -0,0 +1,16 @@
+extends Node
+
+# Exclusively for end game scene storage for now
+
+var endgame_money
+var endgame_morality
+var endgame_opinion
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
